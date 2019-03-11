bb6b21997a9e7ba2db54c173526b0f41f111b54d
mario <axereliat@gmail.com>
2019-03-11 15:54:42 +0200

    Minor update

--- src/api/requester.js
+++ src/api/requester.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseUrl = 'http://localhost:8000/';
+// const baseUrl = 'http://localhost:8000/';
+const baseUrl = 'https://instakilogram-rest.herokuapp.com/';
 
 export class Requester {
 
